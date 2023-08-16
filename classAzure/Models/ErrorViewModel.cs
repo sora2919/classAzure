3653fc411c62ef04277d46b32c8fdffa7a9b5e78
sora2919 <sora2919@gmail.com>
2023-08-16 16:42:52 +0800

    加入專案檔案。

---
+++ classAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace classAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
